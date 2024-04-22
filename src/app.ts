70d798c4fc4fb4837f9ca7692c4a1f04724c07fd
vladimer <gshatakishvili@gmail.com>
2024-04-23 01:21:10 +0400

    Read env vars from

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import appSetup from './startup/init';
 import routerSetup from './startup/router';
 import securitySetup from './startup/security';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 appSetup(app);
 securitySetup(app, express);
 routerSetup(app);
