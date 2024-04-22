47c9d0184014c1cc59a37d4e2032a6f84dcc6013
vladimer <gshatakishvili@gmail.com>
2024-04-23 01:23:28 +0400

    Read port from

--- src/startup/init.ts
+++ src/startup/init.ts
@@ -3,7 +3,7 @@ import { Express } from 'express';
 const appSetup = (app: Express) => {
     // set database connections
 
-    const APP_PORT = 8000;
+    const APP_PORT = process.env.APP_PORT;
 
     app.listen(APP_PORT, () => {
         console.log(`Server started on port ${APP_PORT}`);
