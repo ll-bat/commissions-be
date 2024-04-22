e27b7eb9e3bdc2919be8edec0a9855f3674b35e2
vladimer <gshatakishvili@gmail.com>
2024-04-23 03:00:02 +0400

    Register controllers in the `router.ts` file

--- src/startup/router.ts
+++ src/startup/router.ts
@@ -1,8 +1,14 @@
-import { Express, Request, Response } from 'express';
+import {Express, Request, Response} from 'express';
+import productsRouter from '../controllers/product.controller';
+import ordersRouter from '../controllers/order.controller';
+import staffMembersRouter from '../controllers/staffMember.controller';
 
 const routerSetup = (app: Express) =>
     app.get('/', async (req: Request, res: Response) => {
-        res.send('Hello Express APIvantage!');
-    });
+        return res.send("Hei there!");
+    })
+        .use('/api/orders', ordersRouter)
+        .use('/api/products', productsRouter)
+        .use('/api/staffMembers', staffMembersRouter)
 
 export default routerSetup;
