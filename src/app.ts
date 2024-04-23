6321479539ced19b8b306db93f6f89b92a4fb863
vladimer <gshatakishvili@gmail.com>
2024-04-23 18:51:35 +0400

    Move env reader to a different file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+
 const app = express();
+import "./loadDotenv"
 import appSetup from './startup/init';
 import routerSetup from './startup/router';
 import securitySetup from './startup/security';
-
-import dotenv from 'dotenv';
-dotenv.config();
 
 appSetup(app);
 securitySetup(app, express);
