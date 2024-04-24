5064f1d18a229da6b44d61b3de4466c186b3ae95
vladimer <gshatakishvili@gmail.com>
2024-04-24 18:55:56 +0400

    Add TODO

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,7 +1,6 @@
 import {Router, Request, Response} from 'express';
 import {useTypeORM} from '../db/postgresql/typeorm';
 import {OrderEntity} from "../db/postgresql/entity/order.entity";
-import {StaffMemberEntity} from "../db/postgresql/entity/staffMember.entity";
 import {Between} from "typeorm";
 
 const controller = Router();
@@ -9,6 +8,8 @@ const controller = Router();
 controller
     .get('/', async (req: Request, res: Response) => {
         const {startDate: startDateString, endDate: endDateString, staffMemberId} = req.query;
+
+        // TODO - do validation of the query parameters
 
         const fromDate = new Date(startDateString as string);
         const endDate = new Date(endDateString as string);
